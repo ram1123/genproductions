2c28f528d4c18de5663f9f33a37697a7eaaac05a
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2022-12-16 10:14:04 +0100

    Updated cards for WW aTGC

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -137,11 +137,14 @@ make_gridpack () {
     
     MG_EXT=".tar.gz"
     # https://launchpad.net/mg5amcnlo/3.0/3.4.x/+download/MG5_aMC_v2.9.12.tar.gz
-    MG=MG5_aMC_v2.9.12$MG_EXT
+    #MG=MG5_aMC_v2.9.12$MG_EXT
     #MGTAR=MG5aMC_LTS_2.9.12$MG_EXT # workaround for LTS
-    MGTAR=MG5_aMC_v2.9.12.tar.gz
+    #MGTAR=MG5_aMC_v2.9.12.tar.gz
     #MGSOURCE=https://cms-project-generators.web.cern.ch/cms-project-generators/$MGTAR
-    MGSOURCE=https://launchpad.net/mg5amcnlo/3.0/3.4.x/+download/$MGTAR
+    #MGSOURCE=https://launchpad.net/mg5amcnlo/3.0/3.4.x/+download/$MGTAR
+    MG=MG5_aMC_v2.9.9$MG_EXT
+    MGTAR=MG5aMC_LTS_2.9.9$MG_EXT # workaround for LTS
+    MGSOURCE=https://cms-project-generators.web.cern.ch/cms-project-generators/$MGTAR
     
     MGBASEDIRORIG=$(echo ${MG%$MG_EXT} | tr "." "_")
     isscratchspace=0
